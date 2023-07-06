b7d392ca18852de3f89ebcff8abe113a838372fc
Hodoronk <tanasa.andrei@hotmail.com>
2023-07-06 16:20:57 +0300

    removed logic dublicates in game object

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let player1 = '';
 let player2 = '';
+let playable = 0;
 
 
 const playerName = document.querySelector('#playerName');
@@ -8,9 +9,11 @@ submitter.addEventListener('click', function(){
     if(player1 === ''){
     player1 = playerFactory(playerName.value, 'X');
     console.log(`Player one name is ${player1.name}, and his symbol is ${player1.symbol}`);
+
     }else if(player2 === ''){
         player2 = playerFactory(playerName.value, 'O');
-        console.log(`Player one name is ${player2.name}, and his symbol is ${player2.symbol}`);
+        console.log(`Player two name is ${player2.name}, and his symbol is ${player2.symbol}`);
+        playable = 1;
     }else{
         console.log('You already have 2 players!');
     }
@@ -23,16 +26,32 @@ const gameBoard = (() => {
             gridspace.textContent = gameboard[index];
         });
     }
+    const winningCombinations = [
+        [0,1,2], [3,4,5], [6,7,8],    //rows
+        [0,3,6], [1,4,5], [2,5,8],   //columns
+        [0,4,8], [2,4,6]            //diagonals
+    ];
 
     const gameboard = ['', '', '', '', '', '', '', '', ''];
     const getGameboard = () => gameboard;
     const updateCell = (index, symbol) => {
         gameboard[index] = symbol;
     };
+    function checkCombination(){ 
+        
+        if(!gameboard.includes('')){
+            alert('TIE!');
+        }
+    }
+
+
+    
     return{
         renderGameSpace,
         getGameboard,
         updateCell,
+        checkCombination,
+        
     };
 })();
 
@@ -44,14 +63,31 @@ const playerFactory = (name, symbol) => {
 }
 
 const game = (() =>{
-
     let turnCount = 0;
+
+    function play(symbol, index){
+        gameBoard.updateCell(index, symbol);
+        console.log(gameBoard.getGameboard());
+        gameBoard.renderGameSpace();
+        gameBoard.checkCombination();
+        turnCount++;
+    }
         const gridSpaces = document.querySelectorAll('.gridspace');
+
         gridSpaces.forEach((gridspace, index) => {
         gridspace.addEventListener('click', function(){
-            if(turnCount === 0 || turnCount === 2 || turnCount === 4 || turnCount === 6 || turnCount === 8){
-                gameBoard.updateCell(index, 'X');
+            if(playable === 1 && turnCount === 0 || turnCount === 2 || turnCount === 4 || turnCount === 6 || turnCount === 8){
+                play('X', index);
+            }else if(playable === 1){
+                play('O', index);
             }
         });
     });
+
+
 })();
+
+
+
+
+
